e6e1248a9bbf621ac9339f52e0abf6410aabff5a
Shiro Miminashi Nohara <s.nohara.2001@gmail.com>
2014-04-27 02:33:23 +0900

    about page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   post 'members/fight' => 'members#fight'
   resources :members
 
+  get '/about' => 'pages#about'
+  get '/ranking' => 'members#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
